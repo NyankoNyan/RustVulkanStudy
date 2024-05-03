413e450a7a1c0aa5419b9ef5ef3cea7251596f7c
Your Name <you@example.com>
2024-05-03 17:09:58 +0300

    More files

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code, unused_variables)]
 use cgmath::vec3;
 
 type VertexType = u16;
@@ -15,16 +16,6 @@ pub static CUBE_VERTICES: [cgmath::Vector3<f32>; 8] = [
 
 // untested
 pub static CUBE_INDICES: &[VertexType] = &[
-    0, 1, 2,
-    2, 3, 0,
-    4, 5, 6,
-    6, 7, 4,
-    0, 2, 4,
-    4, 6, 0,
-    1, 3, 5,
-    5, 7, 1,
-    0, 1, 4,
-    4, 5, 0,
-    2, 3, 6,
-    6, 7, 2,
+    0, 1, 2, 2, 3, 0, 4, 5, 6, 6, 7, 4, 0, 2, 4, 4, 6, 0, 1, 3, 5, 5, 7, 1, 0, 1, 4, 4, 5, 0, 2, 3,
+    6, 6, 7, 2,
 ];
